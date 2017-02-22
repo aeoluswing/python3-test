3ec7501da7f9fee8dbb791f44ce970151bcfddcb
aeolus <aeoluswing@163.com>
2017-02-22 14:39:21 +0800

    更新print_unicode_ans.py

--- scripts/print_unicode_ans.py
+++ scripts/print_unicode_ans.py
@@ -4,10 +4,13 @@
 import sys,unicodedata
 
 def print_unicode_table(words):
-    print("decimal     hex  chr {0:^40}".format("name"))
-    print("-------    ----- --- {0:-<40}".format(""))
+    filename = "unicode-table.txt"
+    fh = open(filename, "w", encoding="utf8")
+    fh.write("decimal     hex  chr {0:^40}".format("name"))
+    fh.write("-------    ----- --- {0:-<40}".format(""))
+
     code = ord(" ")
-    end = 127
+    end = min(0xD800, sys.maxunicode)
 
     while code < end:
         c = chr(code)
@@ -16,11 +19,18 @@ def print_unicode_table(words):
         for word in words:
             if word not in name.lower():
                 result = False
+                break
         if result:
-            print("{0:7} {0:5X} {0:^3c} {1}".format(code,name.title()))
+            #if use print,a unicodeTypeError will raise because of the auto transforation to system default encoding.insead,use write
+            fh.write("{0:7} {0:5X} {0:^3c} {1}".format(code,name.title()))
         code += 1
 
 if __name__ == '__main__':
+    #print("locale.getdefaultlocale():",locale.getdefaultlocale())
+    print("sys.getfilesystemencoding():",sys.getfilesystemencoding())
+    print("stdout.encoding:",sys.stdout.encoding)
+    print("stdin.encoding:",sys.stdin.encoding)
+    print("sys.getdefaultencoding():",sys.getdefaultencoding())
     words = []
     if len(sys.argv) > 1:
         if sys.argv[1] in ("-h","--help"):
@@ -28,6 +38,6 @@ if __name__ == '__main__':
             words = None
         else:
             for word in sys.argv[1:]:
-                words.append(word)
-    if words:
+                words.append(word.lower())
+    if words is not None:
         print_unicode_table(words)
