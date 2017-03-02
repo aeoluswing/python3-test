593cf775ae3bb8e9941318dda0d606247a52c544
jiangyu <jiangyu@enjoyor.net>
2017-03-02 10:39:29 +0800

    新增第三章课后习题

---
+++ scripts/generate_usernames_ans.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# Copyright (c) 2008-11 Qtrac Ltd. All rights reserved.
+# This program or module is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version. It is provided for educational
+# purposes and is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+
+import collections
+import sys
+
+
+ID, FORENAME, MIDDLENAME, SURNAME, DEPARTMENT = range(5)
+
+User = collections.namedtuple("User",
+                              "username forename middlename surname id")
+
+
+def main():
+    if len(sys.argv) == 1 or sys.argv[1] in {"-h", "--help"}:
+        print("usage: {0} file1 [file2 [... fileN]]".format(
+            sys.argv[0]))
+        sys.exit()
+
+    usernames = set()
+    users = {}
+    for filename in sys.argv[1:]:
+        for line in open(filename, encoding="utf8"):
+            line = line.rstrip()
+            if line:
+                user = process_line(line, usernames)
+                users[(user.surname.lower(), user.forename.lower(),
+                       user.id)] = user
+    print_users(users)
+
+
+def process_line(line, usernames):
+    fields = line.split(":")
+    username = generate_username(fields, usernames)
+    user = User(username, fields[FORENAME], fields[MIDDLENAME],
+                fields[SURNAME], fields[ID])
+    return user
+
+
+def generate_username(fields, usernames):
+    username = ((fields[FORENAME][0] + fields[MIDDLENAME][:1] +
+                 fields[SURNAME]).replace("-", "").replace("'", ""))
+    username = original_name = username[:8].lower()
+    count = 1
+    while username in usernames:
+        username = "{0}{1}".format(original_name, count)
+        count += 1
+    usernames.add(username)
+    return username
+
+
+def print_users(users):
+    namewidth = 32
+    usernamewidth = 9
+    col_space = " " * 2
+
+    head_line1 = "{0:<{nw}} {1:^6} {2:{uw}}".format(
+        "Name", "ID", "Username", nw=namewidth, uw=usernamewidth)
+
+    head_line2 = "{0:-<{nw}} {0:-<6} {0:-<{uw}}".format(
+        "", nw=namewidth, uw=usernamewidth)
+
+    header = (head_line1 + col_space + head_line1 + "\n" +
+              head_line2 + col_space + head_line2)
+
+    lines = []
+    for key in sorted(users):
+        user = users[key]
+        initial = ""
+        if user.middlename:
+            initial = " " + user.middlename[0]
+        name = "{0.surname}, {0.forename}{1}".format(user, initial)
+        lines.append("{0:.<{nw}.{nw}} ({1.id:4}) {1.username:{uw}}".format(
+            name, user, nw=namewidth, uw=usernamewidth))
+
+    lines_per_page = 64
+    line_no = 0
+    count = 0
+
+    for left, right in zip(lines[::2],lines[1::2]):
+        if line_no == 0:
+            print(header)
+        print(left + col_space + right)
+        line_no += 1
+        if line_no == lines_per_page:
+            print("\f")
+            line_no = 0
+        count += 2
+    if len(lines) != count:
+        print(lines[-1])
+main()
