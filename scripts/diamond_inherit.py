bd8f8bd72527911e0752d1eab173d8f6d422eca1
jiangyu <jiangyu@enjoyor.net>
2017-03-20 13:51:57 +0800

    新增diamond_inherit.py

---
+++ scripts/diamond_inherit.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class A():
+    def __init__(self):
+        print("class name: ",self.__class__.__name__)
+
+class B(A):
+    pass
+
+class C(A):
+    def __init__(self):
+        print("class name: C")
+
+class D(B,C):
+    def __init__(self):
+        return super(__class__,self).__init__()
+
+if __name__=='__main__':
+    objd = D()
+    print(objd.__class__.__mro__)
